2cc9bd832a9f322be1c956ff4f1273cd9b3075fe
符高松 <1798440312@qq.com>
2018-07-27 10:23:22 +0800

    有问题

--- index.php
+++ index.php
@@ -1,4 +1,4 @@
-<?php
+<!-- <?php
 // +----------------------------------------------------------------------
 // | ThinkPHP [ WE CAN DO IT JUST THINK ]
 // +----------------------------------------------------------------------
@@ -7,17 +7,18 @@
 // | Licensed ( http://www.apache.org/licenses/LICENSE-2.0 )
 // +----------------------------------------------------------------------
 // | Author: liu21st <liu21st@gmail.com>
-// +----------------------------------------------------------------------
+// +---------------------------------------------------------------------- -->
 
-// [ 应用入口文件 ]
-namespace think;
+// // [ 应用入口文件 ]
+// namespace think;
 
-define("APP_PATH", __DIR__.'/application/');
-// 加载基础文件
-require __DIR__ . '/thinkphp/base.php';
+// define("APP_PATH", __DIR__.'/application/');
+// // 加载基础文件
+// require __DIR__ . '/thinkphp/base.php';
 
-// 支持事先使用静态方法设置Request对象和Config对象
+// // 支持事先使用静态方法设置Request对象和Config对象
 
 
-// 执行应用并响应
-Container::get('app', ['APP_PATH'])->run()->send();
+// // 执行应用并响应
+// Container::get('app', ['APP_PATH'])->run()->send();
+// ?>
